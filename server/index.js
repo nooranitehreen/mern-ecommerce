9ad00cb2903f842ac670d20d3b92cb66634080ab
nooranitehreen <ntehreen.1999@gmail.com>
2024-01-15 19:43:41 +0530

    Update the MONGODB url

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 
 //Database Connection With MongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
     console.log("Connected to MongoDB");
  })
