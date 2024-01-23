bc7ca620c384d881e01c9f3156ca49b6fb16043b
nooranitehreen <ntehreen.1999@gmail.com>
2024-01-23 15:07:36 +0530

    chnages updated

--- server/index.js
+++ server/index.js
@@ -47,7 +47,7 @@ app.use('/images/assets', express.static('admin/src/upload/assets'));
 app.post("/upload", upload.single('product'), (req, res) => {
     res.json({
         success : 1,
-        image_url: `https://ecommerce-app-b81s.onrender.com/images/assets/${req.file.filename}`,
+        image_url: `https://ecommerce-app-b81s.onrender.com/images/${req.file.filename}`,
         asset_url: `https://ecommerce-app-b81s.onrender.com/images/assets/${req.file.filename}`
     });
 });
