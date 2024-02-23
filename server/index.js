9cf0889f56691999ed5aaf82a71fbd97b7753e16
nooranitehreen <ntehreen.1999@gmail.com>
2024-02-23 19:49:58 +0530

    updated commit

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 const bcrypt = require("bcrypt");
 
 app.use(cors());
-app.use(express.json());
+app.use(express.static('public', { maxAge: 31536000000 }));
 
 
 //Database Connection With MongoDB
