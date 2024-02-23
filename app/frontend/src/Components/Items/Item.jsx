df753d4b84bcebe220c12237dd0899feab3fc987
nooranitehreen <ntehreen.1999@gmail.com>
2024-02-23 21:26:07 +0530

    my updated final commit

--- app/frontend/src/Components/Items/Item.jsx
+++ app/frontend/src/Components/Items/Item.jsx
@@ -8,7 +8,9 @@ const Item = (props) => {
       <Link to={`/product/${props.id}`}>
         <img onClick={window.scrollTo(0, 0)} src={props.image} alt="" className='image-size' />
       </Link>
+      <Link to={`/product/${props.id}`}>
       <p onClick={window.scrollTo(0, 0)}>{props.name}</p>
+      </Link>
       <div className="item-prices">
         <div className="item-price-new">
             ${props.new_price}
